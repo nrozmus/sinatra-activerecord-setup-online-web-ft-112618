9ae195fe264f17399731f12194d3f25265fce0ca
Nicholas Rozmus <rozmusnicholas@gmail.com>
2019-01-30 20:31:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,3 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 end
+   gem 'sinatra'
+    gem 'activerecord', '4.2.5'
+    gem 'sinatra-activerecord'
+    gem 'rake'
+    gem 'thin'
+    gem 'require_all'
